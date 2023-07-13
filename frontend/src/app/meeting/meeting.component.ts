9e05d78fb91958c4294059196d4c2ffdcbeca05e
kksseni <108082922+kksseni@users.noreply.github.com>
2023-07-14 00:25:43 +0300

    Add chime sdk

--- frontend/src/app/meeting/meeting.component.ts
+++ frontend/src/app/meeting/meeting.component.ts
@@ -1,5 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MeetService} from "./meet.service";
+import { MeetingSessionConfiguration } from "amazon-chime-sdk-js";
+import {
+  useMeetingManager,
+  useMeetingEvent,
+  useAudioVideo,
+  useLocalVideo,
+  ControlBar,
+  ControlBarButton,
+  Remove,
+  Meeting,
+  LeaveMeeting,
+  AudioInputControl,
+  DeviceLabels,
+  VideoInputControl,
+  AudioOutputControl,
+  MeetingStatus,
+  RemoteVideos,
+  Input,
+  useMeetingStatus,
+  LocalVideo
+} from "amazon-chime-sdk-component-library-react";
 /*import * as ChimeSDK from 'aws-sdk';
 import {ChimeSDKMeetings} from "aws-sdk";*/
 @Component({
@@ -8,17 +29,33 @@ import {ChimeSDKMeetings} from "aws-sdk";*/
   styleUrls: ['./meeting.component.css']
 })
 export class MeetingComponent implements OnInit {
-
+  @ViewChild('videoElement') elementRef: ElementRef | undefined;
   constructor(private meetService: MeetService) { }
-
+  //_streamManager: StreamManager;
   ngOnInit(): void {
+  }
+  ngAfterViewInit() {
+    //this._streamManager.addVideoElement(this.elementRef.nativeElement);
   }
 
   startMeet() {
     let name = localStorage.getItem("name")
     this.meetService.startMeet(name).subscribe((res:Object)=>{
-      console.log("res =" + res)
-      //const configuration = new ChimeSDK.ChimeSDKMeetings();
+      // @ts-ignore
+      console.log("res =" +  JSON.stringify(res))
+      const result = JSON.stringify(res);
+
+      // @ts-ignore
+      const meetingSessionConfiguration = new MeetingSessionConfiguration(
+        // @ts-ignore
+        result.data.Meeting,
+        // @ts-ignore
+        result.data.Attendee
+      );
+      const meetingOptions = {
+        deviceLabels: DeviceLabels.AudioAndVideo
+      };
+      
     })
 
   }
